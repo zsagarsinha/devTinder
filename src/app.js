ae0e17dc77cb3a49bbab80bc7a226202f095e977
Sagar Sinha <sagarsinhaa07@gmail.com>
2026-09-12 12:37:00 +0530

    exploring GET,POST and DELETE

--- src/app.js
+++ src/app.js
@@ -3,13 +3,19 @@ const express = require('express');
 const app = express();
 
 
+app.get("/user",(req, res) => {
+    res.send({firstName: "Sagar", lastname: "Sinha"})
+});
 
+app.post("/user",(req, res) => {
+    res.send("Data has been saved successfully");
+});
 app.use("/code",(req, res) => {
     res.send("Baby ko bass pasand hai ");
 });
-app.use("/",(req, res) => {
-    res.send("Namaste from the server");
-});
+// app.use("/",(req, res) => {
+//     res.send("Namaste from the server");
+// });
 app.use("/kemcho",(req, res) => {
     res.send("Kem cho bhaii, maja ma?");
 });
